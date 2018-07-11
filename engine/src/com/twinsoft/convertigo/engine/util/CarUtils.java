07c30424bc6e185332105d7397a31353fd1783b2
Nicolas Albert <nicolas.albert@gmail.com>
2018-07-11 09:13:56 +0200

    Flat properties and yaml like output.

--- engine/src/com/twinsoft/convertigo/engine/util/CarUtils.java
+++ engine/src/com/twinsoft/convertigo/engine/util/CarUtils.java
@@ -26,9 +26,15 @@ import java.io.StringWriter;
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
@@ -38,6 +44,8 @@ import org.apache.commons.io.output.FileWriterWithEncoding;
 import org.apache.commons.lang.StringUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.traversal.NodeIterator;
 
 import com.twinsoft.convertigo.beans.core.DatabaseObject;
 import com.twinsoft.convertigo.beans.core.Project;
@@ -174,6 +182,285 @@ public class CarUtils {
 		}
 	}
 	
+	private static class FlatAttributes {
+		Transformer transformer;
+		DOMSource source;
+		StreamResult result;
+		StringWriter writer;
+		StringBuilder sb;
+		Pattern pFind;
+		
+		FlatAttributes() throws Exception {
+			transformer = TransformerFactory.newInstance().newTransformer();
+			transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+			writer = new StringWriter();
+			result = new StreamResult(writer);
+			source = new DOMSource();
+			sb = new StringBuilder();
+			pFind = Pattern.compile("((?:<.*? )|(?:\\w+=\".*?\"))");
+		}
+		
+		void startElement(Element elt, int depth) throws Exception {
+			source.setNode(elt);
+			transformer.transform(source, result);
+			String txt = writer.toString();
+			writer.getBuffer().setLength(0);
+			String pad = StringUtils.repeat("   ", depth);
+			Matcher mFind = pFind.matcher(txt);
+			while (mFind.find()) {
+				sb.append('\n').append(pad).append(mFind.group(1));
+			}
+			sb.append(">\n");
+		}
+		
+		void stopElement(Element elt, int depth) {
+			sb.append(StringUtils.repeat("   ", depth)).append("</").append(elt.getTagName()).append(">\n");
+		}
+	}
+	
+	private static Document exportProjectFlatAttributes(Project project, final List<TestCase> selectedTestCases) throws Exception {
+		FlatAttributes fa = new FlatAttributes();
+		
+		final Document document = XMLUtils.getDefaultDocumentBuilder().newDocument();
+		//            ProcessingInstruction pi = document.createProcessingInstruction("xml", "version=\"1.0\" encoding=\"UTF-8\"");
+		//            document.appendChild(pi);
+		final Element rootElement = document.createElement("convertigo");
+		
+		rootElement.setAttribute("version", com.twinsoft.convertigo.engine.Version.fullProductVersion);
+		rootElement.setAttribute("engine", com.twinsoft.convertigo.engine.Version.version);
+		rootElement.setAttribute("beans", com.twinsoft.convertigo.beans.Version.version);
+		String studioVersion = "";
+		try {
+			Class<?> c = Class.forName("com.twinsoft.convertigo.eclipse.Version");
+			studioVersion = (String)c.getDeclaredField("version").get(null);
+		} catch (Exception e) {
+		} catch (Throwable th) {
+		}
+		
+		rootElement.setAttribute("studio", studioVersion);
+		document.appendChild(rootElement);
+		
+		final int[] depth = {0};
+		
+		fa.startElement(rootElement, depth[0]);
+		
+		new WalkHelper() {
+			protected Element parentElement = rootElement;
+			
+			@Override
+			protected void walk(DatabaseObject databaseObject) throws Exception {
+				Element parentElement = this.parentElement;
+				depth[0]++;
+				Element element = parentElement;
+				element = databaseObject.toXml(document);
+				
+				if (databaseObject instanceof TestCase && selectedTestCases.size() > 0) { 
+					if (selectedTestCases.contains((TestCase)databaseObject)) {
+						fa.startElement(element, depth[0]);
+						this.parentElement = element;
+						super.walk(databaseObject);
+						fa.stopElement(element, depth[0]);
+					} 
+				} else {
+					fa.startElement(element, depth[0]);
+					this.parentElement = element;
+					super.walk(databaseObject);
+					fa.stopElement(element, depth[0]);
+				}
+				
+				
+				databaseObject.hasChanged = false;
+				databaseObject.bNew = false;
+				
+				this.parentElement = parentElement;
+				depth[0]--;
+			}				
+			
+		}.init(project);
+		
+		fa.stopElement(rootElement, depth[0]);
+		String fileName = project.getDirPath() + "/flatAttributes.xml";
+		File file = new File(fileName);
+		if (!file.exists()) {
+			file.getParentFile().mkdirs();
+		}
+		
+		FileUtils.write(file, fa.sb.toString(), "UTF-8");
+		
+		return null;
+	}
+	
+	private static Document exportProjectFlatProperties(Project project, final List<TestCase> selectedTestCases) throws Exception {
+		final Document document = XMLUtils.getDefaultDocumentBuilder().newDocument();
+		//            ProcessingInstruction pi = document.createProcessingInstruction("xml", "version=\"1.0\" encoding=\"UTF-8\"");
+		//            document.appendChild(pi);
+		final Element rootElement = document.createElement("convertigo");
+		
+		rootElement.setAttribute("version", com.twinsoft.convertigo.engine.Version.fullProductVersion);
+		rootElement.setAttribute("engine", com.twinsoft.convertigo.engine.Version.version);
+		rootElement.setAttribute("beans", com.twinsoft.convertigo.beans.Version.version);
+		String studioVersion = "";
+		try {
+			Class<?> c = Class.forName("com.twinsoft.convertigo.eclipse.Version");
+			studioVersion = (String)c.getDeclaredField("version").get(null);
+		} catch (Exception e) {
+		} catch (Throwable th) {
+		}
+		
+		rootElement.setAttribute("studio", studioVersion);
+		document.appendChild(rootElement);
+		
+		new WalkHelper() {
+			protected Element parentElement = rootElement;
+			
+			@Override
+			protected void walk(DatabaseObject databaseObject) throws Exception {
+				Element parentElement = this.parentElement;
+				
+				Element element = parentElement;
+				element = databaseObject.toXml(document);
+				
+				Integer depth = (Integer) document.getUserData("depth");
+				if (depth == null) {
+					depth = 0;
+				}
+				
+				if (databaseObject instanceof TestCase && selectedTestCases.size() > 0) { 
+					if (selectedTestCases.contains((TestCase)databaseObject)) {
+						parentElement.appendChild(element);
+					} 
+				} else {
+					parentElement.appendChild(element);
+				}
+				
+				document.setUserData("depth", depth + 1, null);
+				
+				this.parentElement = element;
+				super.walk(databaseObject);
+				
+				document.setUserData("depth", depth, null);
+				
+				databaseObject.hasChanged = false;
+				databaseObject.bNew = false;
+				
+				this.parentElement = parentElement;
+			}				
+			
+		}.init(project);
+		
+		Transformer transformer = TransformerFactory.newInstance().newTransformer();
+		transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+		transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+		String fileName = project.getDirPath() + "/flatProperties.xml";
+		File file = new File(fileName);
+		if (!file.exists()) {
+			file.getParentFile().mkdirs();
+		}
+		
+		boolean isCR = FileUtils.isCRLF();
+		
+		Writer writer = isCR ? new StringWriter() : new FileWriterWithEncoding(fileName, "UTF-8");
+		transformer.transform(new DOMSource(document), new StreamResult(writer));
+		
+		if (isCR) {
+			String content = FileUtils.CrlfToLf(writer.toString());
+			writer = new FileWriterWithEncoding(fileName, "UTF-8");
+			writer.write(content);
+		}
+		
+		writer.close();
+		
+		return document;
+	}
+	
+	private static Document exportProjectYML(Project project, final List<TestCase> selectedTestCases) throws Exception {
+		StringBuilder sb = new StringBuilder();
+		final Map<String, Element> cache = new HashMap<>();
+		final TwsCachedXPathAPI xPathAPI = new TwsCachedXPathAPI();
+		
+		new WalkHelper() {
+			int depth = 0;
+			Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+			
+			@Override
+			protected void walk(DatabaseObject databaseObject) throws Exception {
+				Element element = databaseObject.toXml(document);
+				String classname = element.getAttribute("classname").substring(30); // com.twinsoft.convertigo.beans. = 30
+				String pad = StringUtils.repeat("  ", depth);
+				sb.append(pad).append(classname).append(":\n");
+
+				depth++;
+				pad = StringUtils.repeat("  ", depth);
+				
+				//DatabaseObject newDbo = databaseObject.getClass().newInstance();
+				Element nElement = cache.get(classname);
+				if (nElement == null) {
+					nElement = databaseObject.getClass().newInstance().toXml(document);
+					cache.put(classname, nElement);
+				}
+				
+				Element property = (Element) xPathAPI.selectNode(element, "property[@name=\"name\"]");
+				element.removeChild(property);
+				
+				sb.append(pad).append("name: ").append(xPathAPI.selectNode(property, "*/@value").getTextContent()).append('\n');
+				
+				NodeIterator properties = xPathAPI.selectNodeIterator(element, "property[@name]");
+				property = (Element) properties.nextNode(); 
+				while (property != null) {
+					Node nValue = xPathAPI.selectNode(property, "*/@value");
+					if (nValue != null) {
+						String name = property.getAttribute("name");
+						String value = nValue.getTextContent();
+						String defValue = xPathAPI.selectNode(nElement, "property[@name=\"" + name + "\"]/*/@value").getTextContent();
+						if (!value.equals(defValue)) {
+							if (value.contains("\n")) {
+								value = ">\n " + value.replace("\n", "\n ");
+							}
+							sb.append(pad).append(name).append(": ").append(value).append('\n');
+						}
+					}
+					property = (Element) properties.nextNode();
+				}
+				/*
+				if (databaseObject instanceof TestCase && selectedTestCases.size() > 0) { 
+					if (selectedTestCases.contains((TestCase)databaseObject)) {
+						fa.startElement(element, depth[0]);
+						this.parentElement = element;
+						super.walk(databaseObject);
+						fa.stopElement(element, depth[0]);
+					} 
+				} else {
+					fa.startElement(element, depth[0]);
+					this.parentElement = element;
+					super.walk(databaseObject);
+					fa.stopElement(element, depth[0]);
+				}*/
+				sb.append(pad + "children:\n");
+				int len = sb.length();
+				depth++;
+				super.walk(databaseObject);
+				if (sb.length() == len) {
+					sb.setLength(sb.length() - 10 - pad.length());
+				}
+				databaseObject.hasChanged = false;
+				databaseObject.bNew = false;
+				
+				depth -= 2;
+			}				
+			
+		}.init(project);
+		
+		String fileName = project.getDirPath() + "/project.yaml";
+		File file = new File(fileName);
+		if (!file.exists()) {
+			file.getParentFile().mkdirs();
+		}
+		
+		FileUtils.write(file, sb.toString(), "UTF-8");
+		
+		return null;
+	}
+	
 	private static Document exportProject(Project project, final List<TestCase> selectedTestCases) 
 			throws EngineException {
 		try {
@@ -245,6 +532,10 @@ public class CarUtils {
 				}				
 				
 			}.init(project);
+			
+			exportProjectFlatAttributes(project, selectedTestCases);
+			exportProjectFlatProperties(project, selectedTestCases);
+			exportProjectYML(project, selectedTestCases);
 			
 			return document;
 		} catch(Exception e) {
